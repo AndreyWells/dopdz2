6287d25781f5a70fce738120489f1a959948fc8a
Andrey <vikakaef@inbox.ru>
2024-11-09 02:57:12 +0500

    Добавил использование библиотек

--- main.cpp
+++ main.cpp
@@ -1,9 +1,7 @@
 #include <iostream>
 #include <map>
-#include <fstream>
-#include <vector>
-#include <algorithm>
 #include <windows.h>
+#include <fstream>
 using namespace std;
 
 int main()
@@ -13,26 +11,6 @@ int main()
     vector<int>keys;
     ifstream in("test.txt");
     if (in.is_open()) {
-      string line;
-      while(getline(in, line)) {
-            int key = line[0] - '0';
-            keys.push_back(key);
-            dict[(int) line[0] - '0'] = line.substr(2);
-
       }
-      in.close();
-      for (int key: keys) {
-            cout << key << endl;
-      }
-      sort(keys.begin(), keys.end());
-      cout << " "<< endl;
-      for(int key: keys) {
-            cout << dict[key] << endl;
-      }
-    }
-    else {
-        wcout << L" Файл не открыт " << endl;
-    }
-
     return 0;
 }
