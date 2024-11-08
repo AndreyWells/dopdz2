c405c033fc311d0e1d2f3a6c65a5de384d7df940
Andrey <vikakaef@inbox.ru>
2024-11-09 03:00:52 +0500

    Добавил чтение и присваивание ключа

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <map>
 #include <windows.h>
 #include <fstream>
+#include <vector>
 using namespace std;
 
 int main()
@@ -11,6 +12,14 @@ int main()
     vector<int>keys;
     ifstream in("test.txt");
     if (in.is_open()) {
+            string line;
+      while(getline(in, line)) {
+            int key = line[0] - '0';
+            keys.push_back(key);
+            dict[line[0] - '0'] = line.substr(2);
+
+      }
+      in.close();
       }
     return 0;
 }
